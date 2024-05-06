d947a35bba1ad3ceba43a3deb5c0ee273ff588f0
shub39 <cptnshubham39@gmail.com>
2024-05-06 19:13:36 +0530

    Blur in swaync and rofi

--- hyprland/scripts/waybarRestart.sh
+++ hyprland/scripts/waybarRestart.sh
@@ -4,4 +4,4 @@ killall waybar
 killall swaync
 waybar -c ~/.config/dotfiles/waybar/config -s ~/.config/dotfiles/waybar/style.css &
 swaync -s ~/.config/dotfiles/swaync/style.css -c ~/.config/dotfiles/swaync/config.json &
-
+notify-send --app-name=HOME -i ~/.config/dotfiles/fastfetch/moon.png Hello
