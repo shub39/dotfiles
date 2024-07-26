dbf24c94912bab89f98df7c96c1adb012aa76776
shub39 <cptnshubham39@gmail.com>
2024-07-26 19:15:07 +0530

    Misc Tweaks 🌥️

--- hyprland/scripts/scrcpy.sh
+++ hyprland/scripts/scrcpy.sh
@@ -14,10 +14,10 @@ selected_option=$(echo -e "$options" | rofi -dmenu -config ~/.config/dotfiles/ro
 
 case "$selected_option" in
     "Video")
-      scrcpy -S
+      scrcpy --max-size=1024 --video-codec=h265 --video-bit-rate=6M --audio-bit-rate=128K --max-fps=30 --no-audio
         ;;
     "No Video")
-      scrcpy --no-video
+      scrcpy --no-window
         ;;
     *)
         echo "Invalid selection"
