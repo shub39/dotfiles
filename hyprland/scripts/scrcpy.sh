6c87161515b1f01804193f415874cbfb5e71b94f
shub39 <cptnshubham39@gmail.com>
2024-10-01 10:28:38 +0530

    :sparkles: removed hypridle and added wallpaper

--- hyprland/scripts/scrcpy.sh
+++ hyprland/scripts/scrcpy.sh
@@ -8,19 +8,45 @@ then
     exit
 fi
 
+# Get connected devices
+devices=$(adb devices | awk 'NR>1 && $2=="device" {print $1}')
+device_count=$(echo "$devices" | wc -l)
+
+# If no devices are found, exit the script
+if [ "$device_count" -eq 0 ]; then
+    notify-send "No devices connected."
+    exit 1
+fi
+
+# If multiple devices are connected, prompt to select one
+if [ "$device_count" -gt 1 ]; then
+    selected_device=$(echo "$devices" | rofi -dmenu -config ~/.config/dotfiles/rofi/config.rasi -p "Select Device: ")
+else
+    selected_device=$(echo "$devices")
+fi
+
+# If no device was selected, exit the script
+if [ -z "$selected_device" ]; then
+    echo "No device selected."
+    exit 1
+fi
+
+# Options for SCRCPY modes
 options="Video\nNo Video\nVideo & Audio"
 
+# Prompt to select the SCRCPY mode
 selected_option=$(echo -e "$options" | rofi -dmenu -config ~/.config/dotfiles/rofi/config.rasi -p "Select SCRCPY mode: ")
 
+# Run scrcpy with the selected mode and device
 case "$selected_option" in
     "Video")
-      scrcpy --max-size=1024 --video-codec=h265 --video-bit-rate=6M --audio-bit-rate=128K --max-fps=30 --no-audio
+      scrcpy --serial "$selected_device" --max-size=1024 --video-codec=h265 --video-bit-rate=6M --audio-bit-rate=128K --max-fps=30 --no-audio
         ;;
     "No Video")
-      scrcpy --no-window
+      scrcpy --serial "$selected_device" --no-window
         ;;
     "Video & Audio")
-      scrcpy --max-size=1024 --video-codec=h265 --video-bit-rate=6M --audio-bit-rate=128K --max-fps=30
+      scrcpy --serial "$selected_device" --max-size=1024 --video-codec=h265 --video-bit-rate=6M --audio-bit-rate=128K --max-fps=30
         ;;
     *)
         echo "Invalid selection"
--- hyprland/scripts/scrcpyAudio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SCRCPY
-
-killall scrcpy
-notify-send "SCRCPY is running! but you cant see me."
-scrcpy --no-video
-notify-send "SCRCPY is dead :("
